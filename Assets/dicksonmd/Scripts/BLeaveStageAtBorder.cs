a277abbbb56a673236328b45e7a48ab8f90fc449
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2022-01-23 11:05:56 +0800

    Stage Border

---
+++ Assets/dicksonmd/Scripts/BLeaveStageAtBorder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BLeaveStageAtBorder : MonoBehaviour
+{
+    public void Exit()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/dicksonmd/Scripts/BStageBorder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BStageBorder : MonoBehaviour
+{
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("OnTriggerExit");
+        var behaviour = collider.gameObject.GetComponent<BLeaveStageAtBorder>();
+        if (behaviour != null)
+        {
+            behaviour.Exit();
+        }
+    }
+}
